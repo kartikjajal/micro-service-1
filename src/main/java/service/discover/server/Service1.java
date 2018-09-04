c3d14a257258c24bf7c95a06ccaf00a84782e6da
kartik <kartik@ics-global.in>
2018-09-04 17:35:26 +0530

    zip-kin and admin link implemented

--- src/main/java/service/discover/server/Service1.java
+++ src/main/java/service/discover/server/Service1.java
@@ -1,5 +1,7 @@
 package service.discover.server;
 
+import java.util.logging.Logger;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,23 +14,34 @@ public class Service1 {
 	@Autowired
 	private Service2 service2;
 	
+	private static final Logger LOG = Logger.getLogger(Service1.class.getName());
+	
 	@Value("${server.port}")
     private int serverPort;
 
 	@RequestMapping("/hello")
 	public String hello() {
-		return "hello-from-service-1 with "+serverPort;
+		LOG.info("Before hello()");
+		return "hello-from-service-1 with "+serverPort;		
 	}
 	
 	@RequestMapping("/service2")
 	public String users() {
-		return service2.getAllUsers();
+		LOG.info("Before users()");
+		String str=service2.getAllUsers();
+		LOG.info("After users()");
+		return str;
 	}
 	
 	@RequestMapping("/common")	
 	public String common() {
+		LOG.info("Before common()");
 		return "hello-from-common-service-1 with "+serverPort;
 	}
 
+	@RequestMapping("/admin")
+	public String admin() {
+		return "hello-from-admin-service-1 with "+serverPort;
+	}
 	
 }
